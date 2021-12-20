ef8d5a47efabc08abfa0ec6852179e192aeab539
Ivan Aimufua <ivan.aimufua@atco.com>
2021-12-19 20:03:46 -0700

    update changes to main

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import linkedList from './structures/Linkedlist';
 import stack from './structures/Stack';
-import queue from './structures/Queue'
+import queue from './structures/Queue';
 import twoSum from './problems/twoSum';
 
 linkedList();
@@ -9,5 +9,5 @@ stack();
 console.log('----------------');
 queue();
 console.log('----------------');
-console.log('pairs', twoSum([ 2, 4, 5, 7, 11, 15], 9));
+console.log('pairs', twoSum([2, 4, 5, 7, 11, 15], 9));
 console.log('non working pairs', twoSum([2], 18));
